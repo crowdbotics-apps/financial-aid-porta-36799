ffc58eab6b48340f978c3342cbde81ddd1d620da
Crowdbotics <team@crowdbotics.com>
2022-08-16 05:22:11 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/financial_aid_porta_36799/MainActivity.java
@@ -0,0 +1,15 @@
+package com.financial_aid_porta_36799;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "financial_aid_porta_36799";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "financial_aid_porta_36799",
+  url: "https://financial_aid_porta_36799.botics.co",
+  api: "https://financial_aid_porta_36799.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
